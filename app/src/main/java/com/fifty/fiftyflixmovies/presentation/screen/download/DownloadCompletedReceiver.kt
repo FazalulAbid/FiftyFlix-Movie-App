7a291f7914710403943db5360c4f72f33785b292
Fazalul Abid <fazalulabid007@gmail.com>
2023-03-19 13:15:18 +0530

    Updated broadcast receiver.

--- app/src/main/java/com/fifty/fiftyflixmovies/presentation/screen/download/DownloadCompletedReceiver.kt
+++ app/src/main/java/com/fifty/fiftyflixmovies/presentation/screen/download/DownloadCompletedReceiver.kt
@@ -4,6 +4,7 @@ import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class DownloadCompletedReceiver : BroadcastReceiver() {
 
@@ -11,6 +12,7 @@ class DownloadCompletedReceiver : BroadcastReceiver() {
         if (intent?.action == "android.intent.action.DOWNLOAD_COMPLETE") {
             val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1L)
             if (id != -1L) {
+                Toast.makeText(context, "Download finished.", Toast.LENGTH_SHORT).show()
                 println("Download with $id finished!")
             }
         }
